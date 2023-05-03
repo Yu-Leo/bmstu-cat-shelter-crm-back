099b6a7388283e64c6cbbee5884216fa850723fe
Lev Yuvenskiy <levayu22@gmail.com>
2023-05-03 09:18:15 +0300

    Add 'run' to makefile

--- Makefile
+++ Makefile
@@ -2,7 +2,11 @@ swag-init:
 	swag init -g internal/endpoints/router.go
 .PHONY: swag-init
 
-run:
+d-run:
 	docker build -t go-server .
 	docker run --rm -p 9000:9000 go-server
+.PHONY: d-run
+
+run:
+	go run ./...
 .PHONY: run
--- internal/repositories/room.go
+++ internal/repositories/room.go
@@ -2,7 +2,11 @@ package repositories
 
 import (
 	"context"
+	"strings"
 
+	"github.com/mattn/go-sqlite3"
+
+	"github.com/Yu-Leo/bmstu-cat-shelter-crm-back/internal/errors"
 	"github.com/Yu-Leo/bmstu-cat-shelter-crm-back/internal/models"
 	"github.com/Yu-Leo/bmstu-cat-shelter-crm-back/pkg/sqlitedb"
 )
@@ -26,7 +30,20 @@ func NewSqliteRoomRepository(storage *sqlitedb.Storage) RoomRepository {
 }
 
 func (r *roomRepository) Create(ctx context.Context, rd models.CreateRoomRequest) (roomNumber models.RoomNumber, err error) {
-	return "", nil
+	q := `INSERT INTO rooms (number, status)
+VALUES (?, ?) RETURNING rooms.number;`
+
+	var number models.RoomNumber
+
+	err = r.storage.DB.QueryRowContext(ctx, q, rd.Number, rd.Status).Scan(&number)
+
+	if err != nil {
+		if strings.Contains(err.Error(), sqlite3.ErrConstraintUnique.Error()) {
+			return "", errors.RoomNumberAlreadyExists
+		}
+		return "", err
+	}
+	return number, nil
 }
 
 func (r *roomRepository) GetList(ctx context.Context) (roomsList *[]models.Room, err error) {
